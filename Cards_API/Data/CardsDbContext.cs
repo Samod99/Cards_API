7b5770db23043f80a9b08fa41a0172e0fae16a37
Binusha Samod <110772940+Samod99@users.noreply.github.com>
2023-01-23 14:14:24 +0530

    Initial commit

---
+++ Cards_API/Data/CardsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Cards_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cards_API.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //DBset
+        public DbSet<Card> Cards { get; set; }
+    }
+}
---
+++ Cards_API/Migrations/20230123043545_initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CardholderName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CardNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CVC = table.Column<int>(type: "int", nullable: false),
+                    ExpiryMonth = table.Column<int>(type: "int", nullable: false),
+                    ExpiryYear = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
